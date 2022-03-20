fb752aea0b78a766af43d3ed5338d1d2359a9cde
Freddy Alberto Morales <fredalbert37@outlook.com>
2022-03-20 12:50:20 -0500

    add quasar to project

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).use(Quasar, quasarUserOptions).use(createPinia()).use(router).mount('#app')
---
+++ src/quasar-user-options.js
@@ -0,0 +1,16 @@
+
+import './styles/quasar.sass'
+import iconSet from 'quasar/icon-set/ionicons-v4.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
